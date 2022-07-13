6b9edb485c9d890d3b47a2a02f6be4fff9b006f9
markcaf <marcocafassog@gmail.com>
2022-07-13 17:32:50 +0200

    Created a method to generate with API random mails

--- js/script.js
+++ js/script.js
@@ -3,11 +3,26 @@ const app = new Vue(
         el: '#app',
 
         data: {
+            mail: '',
 
+            listMails: [],
         },
 
         methods: {
-            
+            generateMails() {
+
+                this.listMails = [];
+
+                for (let i = 0; i <= 9; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+                            this.mail = response.data.response;
+                            this.listMails.push(this.mail);
+                            this.mail = '';
+                        })
+                }
+
+            }
         },
 
         created() {
